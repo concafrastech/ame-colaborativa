0f74a88cc96e7c49dbdacb978f02236cd722846f
Vinicius Kremer Santos <primeup.vsantos@bv.com.br>
2024-03-18 08:39:32 -0300

    [fix] Ajustando url de prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiFunctions: "http://127.0.0.1:5001/ametech-7a47d/us-central1",
+  apiFunctions: "https://us-central1-ametech-7a47d.cloudfunctions.net",
 };
